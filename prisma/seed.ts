c0deadbb5d91e2715c7035d0ef78badb6fdd6e20
bantheus <matheuschmidt.b@gmail.com>
2023-11-13 17:21:54 -0300

    ♻️ Refactor: changed name of animal species

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,80 +4,80 @@ const prisma = new PrismaClient();
 
 async function main() {
   try {
-    const especieCachorro = await prisma.especie.create({
+    const especieCaninos = await prisma.especie.create({
       data: {
-        nome: "Cachorro",
-        slug: "cachorros",
+        nome: "Canino",
+        slug: "caninos",
       },
     });
 
-    const cachorros = [
+    const caninos = [
       {
         nome: "Balão",
         slug: "balao",
         sexo: "MACHO",
         peso: 34,
-        foto: "https://unsplash.com/pt-br/fotografias/cao-marrom-de-pelagem-curta-no-campo-de-grama-verde-durante-o-dia-ihHcWBnLPtE",
-        especieId: especieCachorro.id,
+        foto: "https://i.imgur.com/UTpGjN2.jpg",
+        especieId: especieCaninos.id,
       },
       {
         nome: "Pipa",
         slug: "pipa",
         sexo: "FEMEA",
         peso: 26,
-        foto: "https://unsplash.com/pt-br/fotografias/cao-marrom-de-pelagem-curta-no-campo-verde-da-grama-N4k-aRrcH90",
-        especieId: especieCachorro.id,
+        foto: "https://i.imgur.com/Gj6PVVA.jpg",
+        especieId: especieCaninos.id,
       },
       {
         nome: "Pompidou",
         slug: "pompidou",
         sexo: "MACHO",
         peso: 30,
-        foto: "https://unsplash.com/pt-br/fotografias/filhote-de-cachorro-golden-retriever-com-colarinho-azul-HTpmedSyZag",
-        especieId: especieCachorro.id,
+        foto: "https://i.imgur.com/8tmKg49.jpg",
+        especieId: especieCaninos.id,
       },
     ];
 
     await prisma.animal.createMany({
-      data: cachorros,
+      data: caninos,
     });
 
-    const especieGato = await prisma.especie.create({
+    const especieFelinos = await prisma.especie.create({
       data: {
-        nome: "Gato",
-        slug: "gatos",
+        nome: "Felino",
+        slug: "felinos",
       },
     });
 
-    const gatos = [
+    const felinos = [
       {
         nome: "Snowbell",
         slug: "snowbell",
         sexo: "MACHO",
         peso: 6,
-        foto: "https://unsplash.com/pt-br/fotografias/fotografia-de-foco-seletivo-de-gato-branco-FRpJVO1AjlA",
-        especieId: especieGato.id,
+        foto: "https://i.imgur.com/Q34iC22.jpg",
+        especieId: especieFelinos.id,
       },
       {
         nome: "Docinho",
         slug: "docinho",
         sexo: "FEMEA",
         peso: 7,
-        foto: "https://unsplash.com/pt-br/fotografias/gato-de-smoking-no-chao-de-madeira-marrom-vvY5w49SQpM",
-        especieId: especieGato.id,
+        foto: "https://i.imgur.com/FEIKpA1.jpg",
+        especieId: especieFelinos.id,
       },
       {
         nome: "Pepeto",
         slug: "pepeto",
         sexo: "MACHO",
         peso: 8,
-        foto: "https://unsplash.com/pt-br/fotografias/gato-preto-no-chao-branco-j7uVTlpwI0o",
-        especieId: especieGato.id,
+        foto: "https://i.imgur.com/vMWReo3.jpg",
+        especieId: especieFelinos.id,
       },
     ];
 
     await prisma.animal.createMany({
-      data: gatos,
+      data: felinos,
     });
 
     console.log("Dados inseridos com sucesso!");
