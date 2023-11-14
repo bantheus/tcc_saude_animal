9e35bb90d746a8d6a92fde44e2bbef0060cd73c1
bantheus <matheuschmidt.b@gmail.com>
2023-11-13 21:17:02 -0300

    🚧 Feat: added btn to see all animals

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
-import LatestAnimalsCard from "./ui/home-page-skeleton";
+import { Button } from "@/components/ui/button";
+import { ArrowRightIcon } from "lucide-react";
+import LatestAnimalsCard from "./ui/latest-animals-card";
 
 export default function Home() {
   return (
     <>
       <LatestAnimalsCard />
+
+      <Button
+        variant="link"
+        size="sm"
+        className="bg-[#64ccc5] text-white flex items-center gap-2 w-full mt-6"
+      >
+        Ver todos
+        <ArrowRightIcon size={16} />
+      </Button>
     </>
   );
 }
--- src/app/ui/skeletons/skeletons.tsx
+++ src/app/ui/skeletons/skeletons.tsx
@@ -1,9 +1,7 @@
-const pulse = "motion-safe:animate-pulse";
-
 export function HeaderSkeleton() {
   return (
     <>
-      <div className={`flex justify-between items-center ${pulse}`}>
+      <div className="flex justify-between items-center">
         <div className="h-4 w-1/2 bg-gray-200 rounded-sm shadow-sm" />
         <div className="h-6 w-6 bg-gray-200 rounded-sm shadow-sm"></div>
       </div>
@@ -14,7 +12,7 @@ export function HeaderSkeleton() {
 export function CardSkeleton() {
   return (
     <>
-      <div className={`flex flex-col gap-2 mt-2 rounded-lg ${pulse}`}>
+      <div className="flex flex-col gap-2 mt-6 rounded-lg">
         <div className="p-4 shadow-md border-none bg-gray-50 rounded-lg">
           <div className="flex gap-6 items-center p-0 ">
             <div className="w-24 h-16 shadow-sm bg-gray-200 rounded-full" />
@@ -36,16 +34,16 @@ export function CardSkeleton() {
 
 export default function HomePageSkeleton() {
   return (
-    <>
+    <div className="motion-safe:animate-pulse">
       <HeaderSkeleton />
 
-      <div
-        className={`relative ${pulse} overflow-hidden mt-10 w-full h-4 bg-gray-200 shadow-sm rounded-sm`}
-      />
+      <div className="relative overflow-hidden mt-10 w-full h-4 bg-gray-200 shadow-sm rounded-sm" />
 
       <CardSkeleton />
       <CardSkeleton />
       <CardSkeleton />
-    </>
+
+      <div className="bg-gray-200 mt-6 h-8 rounded-sm w-full" />
+    </div>
   );
 }
