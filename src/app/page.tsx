fef177e3c06e59363708ded4e62ae8cd8e884a5d
bantheus <matheuschmidt.b@gmail.com>
2023-11-14 22:35:55 -0300

    ✨ Feat: add pagination component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRightIcon } from "lucide-react";
+import Link from "next/link";
 import LatestAnimalsCard from "./ui/latest-animals-card";
 
 export default function Home() {
@@ -7,14 +8,16 @@ export default function Home() {
     <main className="px-5">
       <LatestAnimalsCard />
 
-      <Button
-        variant="link"
-        size="sm"
-        className="bg-[#64ccc5] text-white flex items-center gap-2 w-full mt-6"
-      >
-        Ver todos
-        <ArrowRightIcon size={16} />
-      </Button>
+      <Link href="/animais">
+        <Button
+          variant="link"
+          size="sm"
+          className="bg-[#64ccc5] text-white flex items-center gap-2 w-full mt-6"
+        >
+          Ver todos
+          <ArrowRightIcon size={16} />
+        </Button>
+      </Link>
     </main>
   );
 }
